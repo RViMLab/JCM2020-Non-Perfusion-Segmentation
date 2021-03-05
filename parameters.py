4fa5d92d6c4b73d647bb75d9037f2ac1e29ee2b9
Joan M. Nunez do Rio <j.m.nunez@ucl.ac.uk>
2021-03-05 00:13:05 +0000

    Cleaning & 'mode' as parameter.

--- parameters.py
+++ parameters.py
@@ -1,10 +1,45 @@
 
 mode = 'metrics' #'inference'#'train' #
 
+# data_folder = 'facnp8'
+# data_path = '../cnp_data/'
 data_folder = 'facnp8'
-data_path = '../cnp_data/'
+data_path = '../exp_cnptool/exp_cnpdata/'
 
-preproc_path = '../cnp_data/facnp860_cv5'#'../cnp_data/cnpdata6b_cv5'#''#'../cnp_data/facnp860_cv5'#''#'../cnp_data/cnpdataX6_split_eq'#../cnp_data/cnpdataX6_split_clahe'#'../cnp_data/cnpdataX1_split'#'../cnp_data/cnpdata_split'#'../cnp_data/cnpdataX1twice_split'#
+# preproc_path = '../cnp_data/facnp860_cv5'#'../cnp_data/cnpdata6b_cv5'#''#'../cnp_data/facnp860_cv5'#''#'../cnp_data/cnpdataX6_split_eq'#../cnp_data/cnpdataX6_split_clahe'#'../cnp_data/cnpdataX1_split'#'../cnp_data/cnpdata_split'#'../cnp_data/cnpdataX1twice_split'#
+preproc_path = '../exp_cnptool/cnp_data/cnpdata7_cv5'
+
+network = 'unet' #'iunet' #
+train_batch_size = 4
+valid_batch_size = 12
+num_epochs = 50001
+
+kcrossval = 5
+nfold = 4 # None for full cross-validation
+
+# Training
+train_augm = True
+train_shuffle = True
+valid_shuffle = False
+test_shuffle = False
+
+nclasses = 2
+input_mode = 'cnp'#'vld'#
+mask_mode = 'vld'#''#
+
+activation = 'softmax'
+loss = 'gdl' #'iterative'#
+optimizer = 'sgd'#'adam'#
+
+# Output
+output_path = 'out'
+model_folder = 'Model'
+model_name = 'model'
+summary_folder = 'Summary'
+train_sm_folder = 'Train'
+valid_sm_folder = 'Valid'
+test_sm_folder = 'Test'
+results_folder = 'results'
 
 # premodel_fdr = './out/out0/premodel' #'' #
 
@@ -21,8 +56,7 @@ weights_folder = r'../cnp_data/facnp8/Valid'
 cv_folder = '../cnp_data/cnpdata6b_cv5'#/fold0/Done' # to know images in fold
 ##
 
-kcrossval = 5
-nfold = 4 # TODO: to be removed
+
 
 visualization_step = 10000000
 valid_visual_step = 10000000
@@ -30,7 +64,7 @@ test_visual_step = 10000000
 save_results = True#False# # check if still actually used
 verbose = False#True#
 
-gpu_list = '3'
+gpu_list = '1'
 
 crop_height = 1400#2000
 crop_width = 1400#2000
@@ -44,33 +78,22 @@ train_fraction = 0.8#0.85#1#0.98#0.70#
 
 # data_augm_rate = 0 # if 0 then no data augmentation enabled
 
-nclasses = 2
-input_mode = 'cnp'#'vld'#
-mask_mode = 'vld'#''#
 
-activation = 'softmax'#'sigmoid'#
-network = 'unet' #'iunet' #
-loss = 'gdl' #'iterative'#
+
+
 # activation = 'softmax'#
 # network = 'unet' #
 # loss = 'gdl' #
 
 # Training
-train_batch_size = 4
-valid_batch_size = 12
 # test_batch_size = 2 # hardwired to test_nelem
-num_epochs = 50001
+
 
 # Model saving
 models_to_keep = 5
 model_save_step = 1000
 
-train_augm = True#False#
-train_shuffle = True#False#
-valid_shuffle = False#True#
-test_shuffle = False#True#
 
-optimizer = 'sgd'#'adam'#
 
 batch_norm = True#False
 start_learning_rate = 0.005#0.001
@@ -82,15 +105,7 @@ staircase = False#True
 # l2_beta = 0.0005
 dropout_keep = 0.5
 
-# Output
-output_path = 'out'
-model_folder = 'Model'
-model_name = 'model'
-summary_folder = 'Summary'
-train_sm_folder = 'Train'
-valid_sm_folder = 'Valid'
-test_sm_folder = 'Test'
-results_folder = 'results'
+
 
 # Validation
 # train_fraction = 0.98#0.70#0.85
